2670551db363fd227b7ee7d7a2b880591fc93b16
hassan-s47 <hhsheikh77@gmail.com>
2021-03-26 19:29:43 +0500

    Index Merger Resolved

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -39,18 +39,15 @@ router.get('/album/detail',(req, res)=>{
 router.get('/student',(req, res)=>{
     res.render('addStudent');
 })
-<<<<<<< HEAD
 router.get('/viewStudent',(req, res)=>{
     res.render('studentManagement')
 })
 router.get('/routine',(req, res)=>{
     res.render('routine')
 })
-=======
 
 router.get('/createConcert',(req, res) =>{
     res.render('createConcert')
 })
 
->>>>>>> 5e6353062b194c7621f212bc52d6b2b40d0364b9
 module.exports = router;
